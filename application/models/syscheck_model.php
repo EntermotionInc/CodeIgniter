029408117bb8a7cbaef4a23b338f31577bf539aa
t-fukuyo <t_fukuyo@entermotion.jp>
2014-08-13 12:33:09 +0900

    [bugfix] 適当に作ったので動かないのを修正
    適当に作ったので動かないのを修正

--- application/models/syscheck_model.php
+++ application/models/syscheck_model.php
@@ -1,16 +1,23 @@
 <?php
 class Syscheck_model extends CI_Model
 {
-    protected $ci;
+    protected $c;
     
     public function __construct()
     {
         parent::__construct();
-        $this->ci =& get_instance();
+        $this->c =& get_instance();
     }
+    
+    /**
+     * getNow
+     *
+     * @params none
+     * @return array $data
+     */
     public function getNow()
     {
-        if (($data = $this->ci->db->query('SELECT NOW()')->result()) === false) {
+        if (($data = $this->c->db->query('SELECT NOW()')->result()) === false) {
           throw new Exception ("getting Data Failed.");
         }
         return $data[0];
